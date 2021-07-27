bf30544364135ec83865328f04c6b9497205d2e4
amy-mavis-qin <amyqin501@gmail.com>
2021-07-26 21:32:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-module.exports = require('./general-chemistry');
-module.exports = require('./physical-chemistry');
+module.exports = require('./general-chemistry/functions.js');
+module.exports = require('./physical-chemistry/functions.js');
 module.exports = require('./definitions');
