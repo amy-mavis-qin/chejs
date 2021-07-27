d8b6a15360ad6ed943d3048d7f589439538d546a
amy-mavis-qin <amyqin501@gmail.com>
2021-07-26 21:36:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports = require('./general-chemistry/functions.js');
 module.exports = require('./physical-chemistry/functions.js');
-module.exports = require('./definitions');
+
+module.exports = require('./definitions/antoine_constants.js');
+module.exports = require('./definitions/element_mass_properties.js');
+module.exports = require('./definitions/units.js');
