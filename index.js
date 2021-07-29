e52fcd92afc8a29777cbfa1a8b4daec3a8ea25d5
amy-mavis-qin <amyqin501@gmail.com>
2021-07-29 18:59:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { default as antoine_constants } from "./src/definitions/antoine_constants.js";
 export { default as element_mass_properties } from "./src/definitions/element_mass_properties.js";
 export { default as units } from "./src/definitions/units.js";
-export { default as functions } from "./src/general-chemistry/functions.js";
+export { default as molarMass } from "./src/general-chemistry/molarMass.js";
 export { default as functions } from "./src/physical-chemistry/functions.js";
--- scripts/index.js
+++ scripts/index.js
@@ -19,6 +19,9 @@ function updateVersion(version) {
             arr[1] = parseInt(arr[1]) + 1;
         } else if (version === 'patch') {
             arr[2] = parseInt(arr[2]) + 1;
+        } else if (!version) {
+            console.log('Skipping update version.');
+            return;
         } else {
             throw new Error(`${version} is invalid.`);
         }
@@ -42,4 +45,36 @@ function updateVersion(version) {
     })
 }
 
+// Update index.js with functions
+function update() {
+    const lines = []; 
+    
+    const src = fs.readdirSync('./src');
+    fs.readFileSync('./index.js', 'utf-8').split(/\r?\n/).forEach(function(line){
+        lines.push(line);
+    })
+
+    src.forEach((dir) => {
+        const files = fs.readdirSync(`./src/${dir}`);
+        files.forEach((file) => {
+            const path = `./src/${dir}/${file}`;
+            const func = file.replace('.js', '');
+
+            if (!lines.find((line) => new RegExp(path).test(line)) && (!lines.find((line) => /\n/.test(line))) ) {
+                fs.appendFileSync('./index.js', `export { default as ${func} } from "${path}";\n`);
+            } else if (!lines.find((line) => /\n/.test(line))) {
+                if (!fs.existsSync(path)) {
+                    const options = {
+                        files: './index.js',
+                        from: `export { default as ${func} } from "${path}";\n`,
+                        to: "",
+                      };
+                    replace.sync(options);
+                }
+            }
+        })
+    })
+}
+
+update();
 updateVersion(opt);
