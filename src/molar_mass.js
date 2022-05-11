fe8e9295a0639c32573672b8460ca2251d2f2b9a
Amy Qin <amyqin501@gmail.com>
2022-05-11 13:18:10 -0400

    Fix molar mass bug

--- src/molar_mass.js
+++ src/molar_mass.js
@@ -3,27 +3,22 @@ const elements = require('./definitions/element_mass_properties.js');
 function molarMass(formula){
     let molarMass = 0;
     const elementArray = formula.split(/(?=[A-Z])/);
-    let foundElement = false;
     for (const i in elements) {
         for (var j in elementArray) {
             if (elements[i].symbol == elementArray[j]) {
                 molarMass = molarMass + elements[i].mass;
-                foundElement = true;
-            }
+            }   
             if (/\d/.test(elementArray[j])) {
                 var subscript = parseFloat((elementArray[j].match(/\d+/))[0], 10);
                 var element = elementArray[j].replace(subscript,'');
                 if(subscript !== 0 && elements[i].symbol == element){
                     molarMass = molarMass + (subscript * elements[i].mass)
-                } 
+                }
             }
-            console.log("j"+j)
-            console.log('molar mass ' + molarMass)
         }
     }
-    if (!foundElement) {
-        console.log('Could not recognize one or more elements. Please ensure capitalization is correct');
-        return;
+    if (molarMass === 0) {
+        return ('Could not recognize element. Please ensure capitalization is correct');
     }
     return molarMass.toFixed(3);
 }
